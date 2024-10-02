2322eb4ade7d9cc37cda90f1651db4d6e0bf5d81
Mohammed <mohammed.malha@ifosup.wavre.be>
2024-10-03 00:54:07 +0200

    Ajout de la route About

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.post('/', (req, res) => {
   res.send('Hello, Express!');
 });
 
+// Route GET pour la page about
+app.get('/about', (req, res) => {
+    res.send('Ceci est la page About.');
+  });
+  
 // Démarrer le serveur sur le port 3000
 app.listen(3000, () => {
   console.log('Serveur démarré sur http://localhost:3000');
